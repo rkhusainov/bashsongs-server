a1db3c4dc4dcae47644f4fd9e45cfe6dc6e653f0
Rinat Khusainov <rinat.khusainov.msk@gmail.com>
2019-09-04 10:30:15 +0300

    Clear unused imports

--- src/main/java/com/github/rkhusainov/bashsongs/controller/SongController.java
+++ src/main/java/com/github/rkhusainov/bashsongs/controller/SongController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/")
